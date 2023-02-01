bde2d7ab5a75de6f2ce6d4c18bef1b426ae27f57
Yeuda Shtern <yeudashtern@gmail.com>
2023-02-01 14:09:14 +0200

    qqq

--- models/commentsModel.js
+++ models/commentsModel.js
@@ -3,7 +3,10 @@ const Joi = require("joi")
 
 const CommentsSchema = new mongoose.Schema({
     name: String,
+    title: String,
     project_id: String,
+    image: String,
+    info: String,
     date_created: {
         type: Date, default: Date.now
     }
@@ -13,7 +16,10 @@ exports.CommentsModel = mongoose.model("comments", CommentsSchema);
 
 exports.validateComments = (_reqBody) => {
     let joiSchema = Joi.object({
-        name: Joi.string().min(2).max(150).required()
+        name: Joi.string().min(2).max(150).required(),
+        title: Joi.string().min(2).max(150).required(),
+        image: Joi.string().min(2).max(9999).allow(null,""),
+         info: Joi.string().min(2).max(2000).allow(null,"")
     })
 
     return joiSchema.validate(_reqBody);
